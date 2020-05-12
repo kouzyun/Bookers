9e5197210806333c5b06c05658a08cab1ead8a72
harakoji05 <harakoji.0514@gmail.com>
2020-05-12 15:55:23 +0900

    [update]バリテーションチェックを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'books#top'
+  root to: 'books#top'
 
   get 'books'  => 'books#index'
+
+  get 'new' => 'books#index'
 
   post "books" => "books#create"
 
